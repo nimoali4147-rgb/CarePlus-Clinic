a4ea3c2a19dde5ac51b984a49742f2d0ca6ac95f
nimoali4147-rgb <nimoali4147@gmail.com>
2026-08-24 16:02:28 +0300

    Initial CareLink setup

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar () {
+  return (
+    <div>
+        <h1>CareLink</h1>
+        <p>Welcome to CareLink Clinic Appoinment</p>
+    </div>
+  )
+}
+
+export default Navbar
